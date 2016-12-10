bfe3037543e0a63c4f9a74f99fb723bfcff66188
Michelle Bui <michellebui@Michelles-MacBook-Pro-4.local>
2016-12-10 01:35:30 -0800

    changed textview color and centered it

--- app/src/main/java/com/example/michellebui/basicapp/MainActivity.java
+++ app/src/main/java/com/example/michellebui/basicapp/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.michellebui.basicapp;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
@@ -14,6 +19,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView text = (TextView) findViewById(R.id.text_view);
+        text.setTextColor(Color.parseColor("#3baac1"));
+        ImageView imageView = (ImageView) findViewById(R.id.image_view);
+        Picasso.with(this)
+                .load("http://s3.amazonaws.com/bit-photos/large/7009224.jpeg")
+                .into(imageView);
     }
 
     /** Called when the user clicks the Send button */
